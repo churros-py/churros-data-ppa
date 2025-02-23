834b7cf6b1e256cff16723ddcac5b7ae26d37f06
Gustavo Valle <gustavo.vallerp@hotmail.com>
2025-02-23 09:44:43 +0100

    :sparkles: feat: add default crud operations

--- app/repository/base.py
+++ app/repository/base.py
@@ -1,34 +1,61 @@
 import re
-from sqlalchemy.orm import Session
-from typing import Type, TypeVar, List
+from sqlmodel import Session, select, SQLModel
+from typing import Type, TypeVar, List, Optional, Generic, cast
 
-T = TypeVar("T")
+T = TypeVar("T", bound=SQLModel)
+ID = TypeVar("ID")
 
-class GenericRepository:
+class BaseRepository(Generic[T]):
     def __init__(self, session: Session, model: Type[T]):
         self.session = session
         self.model = model
 
+    def save(self, entity: T) -> T:
+        self.session.add(entity)
+        self.session.commit()
+        self.session.refresh(entity)
+        return entity
+
+    def find_by_id(self, id: ID) -> Optional[T]:
+        return cast(Optional[T], self.session.get(self.model, id))
+
+    def find_all(self) -> List[T]:
+        return cast(List[T], self.session.exec(select(self.model)).all())
+
+    def update(self, entity: T) -> T:
+        self.session.add(entity)
+        self.session.commit()
+        self.session.refresh(entity)
+        return entity
+
+    def delete(self, model_id: ID) -> Optional[T]:
+        entity = self.find_by_id(model_id)
+        if entity is not None:
+            self.session.delete(entity)
+            self.session.commit()
+        return entity
+
+    def delete_all(self) -> int:
+        query = select(self.model)
+        result = self.session.exec(query).all()
+        deleted_count = len(result)
+        for entity in result:
+            self.session.delete(entity)
+        self.session.commit()
+        return deleted_count
+
+class DynamicQueryMixin(Generic[T]):
     def _generate_query(self, method_name: str, *args) -> List[T]:
         pattern = r"find_by_(\w+)"
         match = re.match(pattern, method_name)
-
         if not match:
             raise AttributeError(f"Method '{method_name}' is not recognized")
-
         field_names = match.group(1).split("_and_")
-
         if len(field_names) != len(args):
             raise ValueError(f"Method '{method_name}' expects {len(field_names)} parameters, but got {len(args)}")
-
         filters = [getattr(self.model, field) == value for field, value in zip(field_names, args)]
-        query = self.session.query(self.model).filter(*filters)
-        
-        result = query.all()
-        print(f"🔍 Executing Query: {query}")  
-        print(f"🔍 Query Result: {result}")  
-
-        return result or []
+        query = select(self.model).where(*filters)
+        return cast(List[T], self.session.exec(query).all())
 
     def __getattr__(self, name: str):
         if name.startswith("find_by_"):
@@ -36,3 +63,6 @@ class GenericRepository:
                 return self._generate_query(name, *args)
             return method
         raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+class ChurrosRepository(BaseRepository[T], DynamicQueryMixin[T]):
+    pass
--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
 import logging
-from app.repository.decorators import Repository
-from app.repository.base import GenericRepository
+from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+from app.repository.base import ChurrosRepository
+from sqlmodel import create_engine, Session, SQLModel, Field
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
@@ -13,31 +11,22 @@ DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: str
 
-Base = declarative_base()
+session = Session(engine)
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-
-Base.metadata.create_all(bind=engine)
-
-@Repository
-class UserRepository(GenericRepository):
-    def __init__(self, session: Session):
-        super().__init__(session, User)
+user_repo = ChurrosRepository[User](session, User)
 
 app = FastAPI()
 
+SQLModel.metadata.create_all(engine)
+
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    with Session(engine) as session:
+        yield session
 
 class UserCreate(BaseModel):
     name: str
@@ -45,30 +34,34 @@ class UserCreate(BaseModel):
 
 @app.post("/users/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    new_user = User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user_repo.save(User(**user.dict()))
+
+@app.get("/users/")
+def get_all_users(db: Session = Depends(get_db)):
+    return user_repo.find_all()
+
+@app.get("/users/id/{id}")
+def get_user_by_id(id: int, db: Session = Depends(get_db)):
+    return user_repo.find_by_id(id)
 
 @app.get("/users/name/{name}")
 def get_users_by_name(name: str, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
-    users = user_repo.find_by_name(name)
-    print(f"🔍 Retrieved users: {users}")
-    return users
+    return user_repo.find_by_name(name)
 
 @app.get("/users/email/{email}")
 def get_users_by_email(email: str, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
     return user_repo.find_by_email(email)
 
-@app.get("/users/id/{id}")
-def get_users_by_id(id: int, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
-    return user_repo.find_by_id(id)
+@app.delete("/users/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    user_repo.delete(id)
+    return {"message": "User deleted successfully"}
+
+@app.delete("/users/")
+def delete_all_users(db: Session = Depends(get_db)):
+    user_repo.delete_all()
+    return {"message": "All users deleted"}
 
 @app.get("/users/name/{name}/email/{email}")
 def get_users_by_name_and_email(name: str, email: str, db: Session = Depends(get_db)):
-    user_repo = UserRepository(db)
     return user_repo.find_by_name_and_email(name, email)
