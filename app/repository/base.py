c75e027c60d5624461d72a55a84943c300250060
Gustavo Valle <gustavo.vallerp@hotmail.com>
2025-02-23 11:06:29 +0100

    ci: add dynamic query mixin unit tests

--- app/repository/base.py
+++ app/repository/base.py
@@ -1,4 +1,3 @@
-import re
 from sqlmodel import Session, select, SQLModel
 from typing import Type, TypeVar, List, Optional, Generic, cast
 
@@ -43,26 +42,3 @@ class BaseRepository(Generic[T]):
             self.session.delete(entity)
         self.session.commit()
         return deleted_count
-
-class DynamicQueryMixin(Generic[T]):
-    def _generate_query(self, method_name: str, *args) -> List[T]:
-        pattern = r"find_by_(\w+)"
-        match = re.match(pattern, method_name)
-        if not match:
-            raise AttributeError(f"Method '{method_name}' is not recognized")
-        field_names = match.group(1).split("_and_")
-        if len(field_names) != len(args):
-            raise ValueError(f"Method '{method_name}' expects {len(field_names)} parameters, but got {len(args)}")
-        filters = [getattr(self.model, field) == value for field, value in zip(field_names, args)]
-        query = select(self.model).where(*filters)
-        return cast(List[T], self.session.exec(query).all())
-
-    def __getattr__(self, name: str):
-        if name.startswith("find_by_"):
-            def method(*args):
-                return self._generate_query(name, *args)
-            return method
-        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
-
-class ChurrosRepository(BaseRepository[T], DynamicQueryMixin[T]):
-    pass
